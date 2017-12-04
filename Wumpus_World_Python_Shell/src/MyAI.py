d6b6f2929a1b8921840b1d17dd43757661600507
Benjamin Max Fallan <bfallan@andromeda-8.ics.uci.edu>
2017-12-03 18:34:27 -0800

    tracker stuff

--- Wumpus_World_Python_Shell/src/MyAI.py
+++ Wumpus_World_Python_Shell/src/MyAI.py
@@ -34,6 +34,10 @@ class MyAI ( Agent ):
         self.orientation_history = [
             (self.position, self.dir, self.action)
         ]
+        self.max_x = 7
+        self.min_x = 0
+        self.max_y = 7
+        self.min_y = 0
         self.post_bump = False
         self.bump_counter = 0
         self.turning = False
@@ -41,9 +45,9 @@ class MyAI ( Agent ):
         self.has_gold = False
         self.wumpus_alive = True
         self.can_shoot = True # always a boolean
-        self.shot_pos = None
+        self.just_shot = False
         self.tile_info = {(0,0) : 1}
-        self.visited_nodes = {(0,0): True}
+        self.tracker = {}
         # ======================================================================
         # YOUR CODE ENDS
         # ======================================================================
@@ -59,11 +63,8 @@ class MyAI ( Agent ):
         }
 
         #pprint(stateDict)
-        #xpprint(self.visited_nodes)
-
         is_dangerous = True if breeze or stench else False
         #pprint(self.orientation_history)
-
         #print("position of last turn:", self.get_position())
         if(self.turning):
             return self.change_dir(self.goal_dir)
@@ -102,69 +103,95 @@ class MyAI ( Agent ):
 
         if(self.tile_info[0,0] > 2):
             return self.climb()
-           
-        
+        if(self.get_position() == (0,0) and self.dir == 's'):
+            return self.climb()
+
         elif(glitter):
             self.has_gold = True
             return self.grab()
             #on the coordinate where Gold is
-        if(scream): 
+        elif(scream): 
             self.wumpus_alive = False
 
         elif(bump):
-            #print("inside bump")
-        #might be popping at wrong time
+        #print("inside bump")
+            
             self.orientation_history.pop()
             self.post_bump = True
             self.recover_position()
-            return self.bump_help()   
+            pos = self.get_position()
+            if(pos[0] <= self.max_x and self.get_dir() == 'e'):
+                self.max_x = pos[0]
+            if(pos[1] <= self.max_y and self.get_dir() == 'n'):
+                self.max_y = pos[1]         
+            if(stench and self.wumpus_alive):
+                self.post_bump = False
+                self.update_goal_dir(self.oppdir(self.get_dir()))
+                return self.change_dir(self.goal_dir)     
+            return self.bump_help()
+
 
         elif(is_dangerous):
+            self.post_bump = False
+            self.bump_counter = 0
             if(self.get_move_count() == 0):
                 if(breeze):
                     return self.climb()
                 elif(stench and self.can_shoot):
-                    self.shot_pos = self.get_position()
                     return self.shoot()
-            elif(self.get_position() == (0,0) and not self.can_shoot and self.tile_info[(0,0)] > 1):
+            elif(self.get_position() == (0,0) and (not self.can_shoot) and (self.tile_info[(0,0)] > 1)):
                 return self.climb()
             elif(breeze):
-                self.post_bump = False
-                self.update_goal_dir(self.oppdir(self.get_dir()))
-                return self.change_dir(self.goal_dir)
+                self.breeze_update_tracker()
+                if(self.tile_info[self.get_position()] <= 1 and not stench):
+                    return self.tracker_based_move()
+                elif(not stench):
+                    return self.get_home_help()
+                else:
+                    self.update_goal_dir(self.oppdir(self.get_dir()))
+                    return self.change_dir(self.goal_dir)
+            #    if(self.open_tile()):
+            #        self.tracker_based_move()
+            #    else:
+            #        self.update_goal_dir(self.oppdir(self.get_dir()))
+            #        return self.change_dir(self.goal_dir)
+                #update dict for possible pits
+                #self.update_pit_tracker()
+                #find num neighbors for current tile for perentage calc of pit chance
+                #self.num_unvisited_neighbors()
+                #check tracker for each tile value to make next move
+                #self.calculated_move()
+                #self.update_goal_dir(self.oppdir(self.get_dir()))
+                #return self.change_dir(self.goal_dir)
             elif(stench):
                 #greedy right now, implement heuristics at higher level
                 if(self.wumpus_alive and self.can_shoot):
-                    self.shot_pos = self.get_position()
                     return self.shoot()
-                elif (self.wumpus_alive and not self.can_shoot and self.shot_pos == self.get_position()):
+                elif(self.wumpus_alive and self.just_shot):
+                    self.just_shot = False
                     return self.move_forward()
-                elif (self.wumpus_alive and not self.can_shoot and self.shot_pos == self.get_position() and self.tile_info[self.position] > 0):
-                    return self.move_forward()
-                elif(self.wumpus_alive and not self.can_shoot):
-                    self.update_goal_dir(self.oppdir(self.get_dir()))
-                    return self.change_dir(self.goal_dir)
-                elif(not self.wumpus_alive and not self.can_shoot):
+                elif(self.wumpus_alive and (self.tile_info[self.get_position()] > 1)):
                     return self.move_forward()
                 else:
                     self.update_goal_dir(self.oppdir(self.get_dir()))
                     return self.change_dir(self.goal_dir)
+               # elif(self.wumpus_alive and not self.can_shoot):
+                #    self.update_goal_dir(self.oppdir(self.get_dir()))
+                 #   return self.change_dir(self.goal_dir)
+                
             else:
                 self.update_goal_dir(self.oppdir(self.get_dir()))
                 return self.change_dir(self.goal_dir)
-                
+
         elif(self.post_bump and self.bump_counter >= 3):
             #handle this stuff in function
             #print("inside post_bump")
-            return self.post_bump_move()
+            #self.update_tracker(breeze)
+            return self.post_bump_move()   
 
-        '''elif(bump):
-            #print("inside bump")
-            self.post_bump = True
-            self.recover_position()
-            return self.bump_help()     '''       
-            
-
+        if(not bump):
+            self.update_tracker()  
+        
         return self.move_forward()
         # ======================================================================
         # YOUR CODE ENDS
@@ -206,6 +233,7 @@ class MyAI ( Agent ):
     def move_forward(self):
         #updates the position and orientation history
         #need to make it so when agent bumps the position doesnt change preemptively 
+        
         if(self.dir == 'e'):
             self.position = (self.position[0] + 1, self.position[1])
         if(self.dir == 'w'):
@@ -261,7 +289,7 @@ class MyAI ( Agent ):
     def shoot(self):
         if(self.can_shoot == True):
             self.can_shoot = False
-            self.orientation_history.append((self.position, self.dir, 'S'))
+            self.just_shot = True
             return Agent.Action.SHOOT
         else:
             print("Can't shoot!")
@@ -311,10 +339,10 @@ class MyAI ( Agent ):
 
     def bump_help(self):
         self.bump_counter += 1
-        if(self.position == (0,0) and self.dir == 'w'):
+        if(self.get_position() == (0,0) and self.dir == 'w'):
             return self.turn_right()
         elif(self.position[0] < self.position[1] and self.dir == 'w' and self.post_bump):
-           return self.turn_right()
+            return self.turn_right()
         elif(self.position[0] > self.position[1] and self.dir == 'e'):
             return self.turn_left()
         elif(self.position[0] > self.position[1] and self.dir == 's'):
@@ -326,6 +354,7 @@ class MyAI ( Agent ):
         elif(self.position[0] == self.position[1] and self.dir == 'e'):
             return self.turn_right()
         elif(self.position[0] == self.position[1] and self.dir == 'n'):
+            self.post_bump = False
             return self.turn_left()
         elif(self.position[0] == self.position[1] and self.dir == 's'):
             return self.turn_left()
@@ -344,18 +373,137 @@ class MyAI ( Agent ):
     def post_bump_move(self):
         self.bump_counter = 0
         self.post_bump = False
+
         if(self.get_position()[0] == self.get_position()[1]):
             return self.turn_left()
+        if(self.get_position()[0] < self.get_position()[1]):
+            return self.turn_right()
+        if(self.get_dir() == 'e'):
+            return self.move_forward()
         if(self.get_dir() == 'w'):
             return self.move_forward()
         if(self.get_dir() == 's'):
             return self.move_forward()   
-        if(self.get_position()[0] < self.get_position()[1]):
-            return self.turn_right()
         elif(self.get_position()[0] > self.get_position()[1]):
             return self.turn_left()
 
-  
+    def update_tracker(self):
+        # look at all 5 possible tiles associated with a move and update tracker
+        x = self.get_position()[0]
+        y = self.get_position()[1]
+      
+        no_pit = (-1, 'NP')
+        basic = (0, 'NP')
+        
+        self.tracker[x,y] = no_pit
+        
+        if(((x+1,y) not in self.tile_info) and (x+1 <= self.max_x)):
+            if((x+1,y) not in self.tracker):
+                self.tracker[x+1,y] = basic
+            else:
+                self.tracker[x+1,y] = (self.tracker[x+1,y][0]+1, self.tracker[x+1,y][1])
+        if(((x-1,y) not in self.tile_info) and (x-1 >= self.min_x)):
+            if((x-1,y) not in self.tracker):
+                self.tracker[x-1,y] = basic
+            else:
+                self.tracker[x-1,y] = (self.tracker[x-1,y][0]+1, self.tracker[x-1,y][1])
+        if(((x,y+1) not in self.tile_info) and (y+1 <= self.max_y)):
+            if((x,y+1) not in self.tracker):
+                self.tracker[x,y+1] = basic
+            else:
+                self.tracker[x,y+1] = (self.tracker[x,y+1][0]+1, self.tracker[x,y+1][1])
+        if(((x,y-1) not in self.tile_info) and (y-1 >= self.min_y)):
+            if((x,y-1) not in self.tracker):
+                self.tracker[x,y-1] = basic
+            else:
+                self.tracker[x,y-1] = (self.tracker[x,y-1][0]+1, self.tracker[x,y-1][1])
+
+        #print(self.tracker)
+
+    def breeze_update_tracker(self):
+        x = self.get_position()[0]
+        y = self.get_position()[1]
+        base = (1, 'PP')
+
+        if(((x+1,y) not in self.tracker) and (x+1 <= self.max_x)):
+            self.tracker[x+1,y] = base
+        elif(((x+1,y) in self.tracker) and (self.tracker[x+1,y][1] != 'NP')):
+            self.tracker[x+1,y] = (self.tracker[x+1,y][0]+1, self.tracker[x+1,y][1])
+        if(((x-1,y) not in self.tracker) and (x-1 >= self.min_x)):
+            self.tracker[x-1,y] = base
+        elif(((x-1,y) in self.tracker) and (self.tracker[x-1,y][1] != 'NP')):
+            self.tracker[x-1,y] = (self.tracker[x-1,y][0]+1, self.tracker[x-1,y][1])
+        if(((x,y+1) not in self.tracker) and (y+1 <= self.max_y)):
+            self.tracker[x,y+1] = base
+        elif(((x,y+1) in self.tracker) and (self.tracker[x,y+1][1] != 'NP')):
+            self.tracker[x,y+1] = (self.tracker[x,y+1][0]+1, self.tracker[x,y+1][1])
+        if(((x,y-1) not in self.tracker) and (y-1 >= self.min_y)):
+            self.tracker[x,y-1] = base
+        elif(((x,y-1) in self.tracker) and (self.tracker[x,y-1][1] != 'NP')):
+            self.tracker[x,y-1] = (self.tracker[x,y-1][0]+1, self.tracker[x,y-1][1])
+
+        #print(self.tracker)
+
+    def tracker_based_move(self):
+        x = self.get_position()[0]
+        y = self.get_position()[1]
+
+        if(((x+1,y) not in self.tile_info) and ((x+1,y) in self.tracker) and (self.tracker[x+1,y][1] == 'NP')):
+            if(self.get_dir() == 'e'):
+                return self.move_forward()
+            else:
+                self.update_goal_dir('e')
+                return self.change_dir(self.goal_dir)
+        if(((x-1,y) not in self.tile_info) and ((x-1,y) in self.tracker) and (self.tracker[x-1,y][1] == 'NP')):
+            if(self.get_dir() == 'w'):
+                return self.move_forward()
+            else:
+                self.update_goal_dir('w')
+                return self.change_dir(self.goal_dir)
+        if(((x,y+1) not in self.tile_info) and ((x,y+1) in self.tracker) and (self.tracker[x,y+1] == 'NP')):
+            if(self.get_dir() == 'n'):
+                return self.move_forward()
+            else:
+                self.update_goal_dir('n')
+                return self.change_dir(self.goal_dir)
+        if(((x,y-1) not in self.tile_info) and ((x,y-1) in self.tracker) and (self.tracker[x,y-1][1] == 'NP')):
+            if(self.get_dir() == 's'):
+                return self.move_forward()
+            else:
+                self.update_goal_dir('s')
+                return self.change_dir(self.goal_dir)
+        else:
+            self.update_goal_dir(self.oppdir(self.get_dir()))
+            return self.change_dir(self.goal_dir)
+
+    def get_home_help(self):
+        x = self.get_position()[0]
+        y = self.get_position()[1]
+        if(x > self.min_x):
+            if(self.dir != 'w'):
+                self.update_goal_dir('w')
+                return self.change_dir(self.goal_dir)
+            else:
+                return self.move_forward()
+        elif(x == self.min_x):
+            self.update_goal_dir('s')
+            return self.change_dir(self.goal_dir)
+        if(y > self.min_y):
+            if(self.dir != 's'):
+                self.update_goal_dir('s')
+                return self.change_dir(self.goal_dir)
+            else:
+                return self.move_forward()
+        elif(y == self.min_y):
+            self.update_goal_dir('w')
+            return self.change_dir(self.goal_dir)
+
+
+    
+
+
+
+
     # ======================================================================
     # YOUR CODE ENDS
     # ======================================================================
